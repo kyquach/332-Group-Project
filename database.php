a5ef000d600d13234b0c25c812e08393fee9cf90
kyquach <kyquach@csu.fullerton.edu>
2024-12-10 12:21:27 -0800

    Database connection established + Error Handling for bad connection

--- database.php
+++ database.php
@@ -1,21 +1,28 @@
 <?php
-
+    //$db_name_from_user                            Take from user_input or automatically create a database?
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "expenses_database";
+    //$db_name = $db_name_from_user                 Use to automate user database generation and connection
+    $db_name = "expensesdb";
     $conn = "";
 
-    $conn = mysqli_connect($db_server,
-                           $db_user, 
+    //Exception Handling
+    try{
+        //Datebase connection
+        $conn = mysqli_connect($db_server,
+                            $db_user, 
                            $db_pass, 
-                           $db_name);    
-    
-    if (!$conn) {
+                           $db_name); 
+    }
+    //If there is an error, echo could not connect.
+        catch(mysqli_sql_exception){
+                echo "Could not connect!";
+        }
+    //Establish that user is connected to the database
+    if ($conn) {
         echo "You are connected!";
     } 
-    else{
-        echo "Could not connect!";
-    }
+    
     
     ?> 
