cde09dcd66e1b397e7b7d4f142861e64654f30ce
kyquach <kyquach@csu.fullerton.edu>
2024-12-09 23:47:15 -0800

    1st commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Welcome";
+    
+    ?>
+    <br>
+    <br>
+
+    <button>Add Expense</button>
+    <button>Delete Expense</button>
+    <button>Edit Expense</button>
+</body>
+</html>
