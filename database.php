d328ac17d2d43560dd2d8e92ab1a15bc01e07dd1
kyquach <kyquach@csu.fullerton.edu>
2024-12-10 01:03:29 -0800

    Database Connection started

--- database.php
+++ database.php
@@ -3,5 +3,19 @@
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "";
+    $db_name = "expenses_database";
     $conn = "";
+
+    $conn = mysqli_connect($db_server,
+                           $db_user, 
+                           $db_pass, 
+                           $db_name);    
+    
+    if (!$conn) {
+        echo "You are connected!";
+    } 
+    else{
+        echo "Could not connect!";
+    }
+    
+    ?> 
