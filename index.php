fc0a28805b76751be134c5b65d12644f952e22e7
shinywesley <130871687+shinywesley@users.noreply.github.com>
2024-12-10 11:02:08 -0800

    Update index.php
    simple table to display expenses from the database

--- index.php
+++ index.php
@@ -1,22 +1,48 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Expenses Tracker</title>
 </head>
 <body>
     <?php
         include("database_functions.php");
         echo "Welcome";
-    
     ?>
-    <br>
-    <br>
+    <br><br>
 
     <button>Add Expense</button>
     <button>Delete Expense</button>
     <button>Edit Expense</button>
+
+    <br><br>
+
+    <h2>Expenses</h2>
+    <?php
+        // fetch data from Expenses
+        $query = "SELECT * FROM Expenses";
+        $result = mysqli_query($conn, $query);
+
+        // display the results if any rows exist
+        if ($result && mysqli_num_rows($result) > 0) {
+            echo "<table border='1'>
+                    <tr>
+                        <th>Expense Name</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                    </tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>
+                        <td>{$row['expense_name']}</td>
+                        <td>{$row['expense_amount']}</td>
+                        <td>{$row['expense_date']}</td>
+                      </tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "<p>No expenses to show.</p>";
+        }
+    ?>
 </body>
 </html>
