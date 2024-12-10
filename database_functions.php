5640f9868ed02f57a5cae25234650682ab590038
kyquach <kyquach@csu.fullerton.edu>
2024-12-10 00:23:21 -0800

    added function variables

---
+++ database_functions.php
@@ -0,0 +1,52 @@
+<?php
+
+
+//Database functions
+//Add Expense Function
+//Give user form with fields to create an expense
+//SQL query pushes created column to the table
+function add_expense($id, $expense_name, $expense_amt, $expense_date_due, ){
+
+    $query = ""
+
+}
+
+//Edit Expense Function
+//Prompt user to edit certain fields in expense
+//SQL query to Pushes edited changes of column to table
+function edit_expense($id, $expense_name, $expense_amt, $expense_date_due){
+    $query = ""
+
+}
+
+
+//Delete Expense Function
+//SQL query to delete certain expense
+//Deletes expense from database
+function delete_expense($id){
+
+    $query = ""
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
